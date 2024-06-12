5a031cfeb52c6540274719ceb28ebb6a67af6178
Minsu Kim <minsu.steven.kim@gmail.com>
2024-06-12 15:40:03 +0900

    Initial commit

---
+++ calc.py
@@ -0,0 +1,21 @@
+class Calc:
+    def __init__(self):
+        pass
+
+    def getSum(self, a, b):
+        pass
+
+    def getGop(self, a, b):
+        pass
+
+    def getZegop(self, a, b):
+        pass
+
+    def getMinus(self, a, b):
+        pass
+
+    def getDivide(self, a, b):
+        pass
+
+    def getSumSum(self, a, b):
+        pass
